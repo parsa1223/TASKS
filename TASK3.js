236ae081dc68b6640969aa0e4e31b0858a526c1e
Parsa Arababadi <arababadiparsa@gmail.com>
2024-06-25 10:20:22 +0330

    123

--- TASK3.js
+++ TASK3.js
@@ -1,4 +1,6 @@
 const input25=document.querySelector('#input3');
+let number=['1','2','3','4','5','6','7','8','9'];
+let num =0;
 function tashkhis2(){
     if(input25.value.length === 5){
         marhale2()
@@ -7,7 +9,16 @@ function tashkhis2(){
         console.log("false")
     }
 };
-function marhale2(){
-let data=input25.value.nextLine();
 
-} 
+function marhale2(){
+    for(let input26 of input25.value){
+        if(number.includes(input26)){
+        num++
+        }
+    }
+    if(num ===5){
+        console.log('true')
+    }else{
+        console.log('false')
+    }
+}
